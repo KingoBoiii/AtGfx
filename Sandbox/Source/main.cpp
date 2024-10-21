a50d8f816a0d20800ffe9d504ca982a1ef500b83
KingoBoiii <kingodk@outlook.dk>
2024-10-21 18:28:44 +0200

    Updated window size

--- Sandbox/Source/main.cpp
+++ Sandbox/Source/main.cpp
@@ -37,7 +37,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(1366, 768, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1600, 900, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
